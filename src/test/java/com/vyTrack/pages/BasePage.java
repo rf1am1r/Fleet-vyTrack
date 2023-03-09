3d8384beb895057e8fc459176416132ca9aa776a
Parviz <parvizguluzadeh@gmail.com>
2023-03-09 10:12:08 -0500

    Step1

--- src/test/java/com/vyTrack/pages/BasePage.java
+++ src/test/java/com/vyTrack/pages/BasePage.java
@@ -19,12 +19,14 @@ import java.util.List;
 
 public abstract class BasePage {
 
-    @FindBy(css = "span.title-level-1")
-    public List<WebElement> menuOptions;
+
 
     @FindBy(css = "div[class='loader-mask shown']")
     @CacheLookup
     protected WebElement loaderMask;
+
+     @FindBy(css = "span.title-level-1")
+    public List<WebElement> menuOptions;
 
     @FindBy(css = "h1[class='oro-subtitle']")
     public WebElement pageSubTitle;
