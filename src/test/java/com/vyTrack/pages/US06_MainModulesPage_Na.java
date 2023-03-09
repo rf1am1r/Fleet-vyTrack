20e072599b8e3bb9474368b1a9267e6fa257131e
nazalimi <nazik.sherzamonova@gmail.com>
2023-03-08 23:55:25 -0500

    Updated

---
+++ src/test/java/com/vyTrack/pages/US06_MainModulesPage_Na.java
@@ -0,0 +1,32 @@
+package com.vyTrack.pages;
+
+import com.vyTrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class US06_MainModulesPage_Na {
+
+    public US06_MainModulesPage_Na(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//span[@class='title title-level-1'][normalize-space()='Fleet']")
+    public WebElement driverFleet;
+   // @FindBy(css = "li[class='dropdown dropdown-level-1 align-menu-right'] span[class='title title-level-1']")
+    //public WebElement driverFleet;
+
+
+    @FindBy(xpath = "//span[normalize-space()='Vehicles']")
+    public WebElement driverVehicle;
+
+
+
+    @FindBy(xpath = "(//div[@class = 'dropdown'])[1]")
+    public WebElement hoverBox;
+
+
+
+
+
+}
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -9,6 +9,28 @@ Feature: Users should be able to login
     Given the user logged in as "driver"
     #Given the user logged in with username as "User1" and password as "UserUser123"
 
+  @store_manager
+  Scenario: Verify login with different user types
+    Given the user logged in as "store manager"
+
+
+  @sales_manager
+  Scenario: Verify login with different user types
+    Given the user logged in as "sales manager"
+
+
+
+  @all_users @wip
+  Scenario Outline: Verify login with different user types
+    Given the user logged in as "<userTypes>"
+
+    Examples:
+      |  userTypes  |
+      |   driver    |
+      |store manager|
+      |sales manager|
+
+
 
 
 
